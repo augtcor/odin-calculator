149447b5e6576d4993956424593c25c54c53e317
augcor <augusto.t.correa@gmail.com>
2022-11-12 12:37:37 -0300

    Add function to store values

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 let displayValue = 0;
+let value = 0;
+
+let display = document.getElementById('display');
+let displayNumber = document.createTextNode(displayValue);
+display.appendChild(displayNumber);
 
 let numbers = document.querySelectorAll('.numbers');
 numbers.forEach(number => {
-    number.addEventListener('click', function () {
-        displayValue = this.id;
-        let display = document.getElementById('display');
-        let displayNumber = document.createTextNode(displayValue);
-        display.appendChild(displayNumber);
+    number.addEventListener('click', function() {
+        if (displayValue === 0) {
+            display.removeChild(displayNumber);
+            displayValue = this.id;
+            displayNumber = document.createTextNode(displayValue);
+            display.appendChild(displayNumber);
+            value += displayValue;
+        } else {
+            displayValue = this.id;
+            displayNumber = document.createTextNode(displayValue);
+            display.appendChild(displayNumber);
+            console.log(displayNumber.nodeValue);
+            value += displayValue;
+        }    
     })
 });
-
-
 
 function add(a, b) {
     return a + b;

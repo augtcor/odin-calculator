e162f93eba739a315a845e48b26aeb6d417690d5
augcor <augusto.t.correa@gmail.com>
2022-11-14 11:46:09 -0300

    Add clear display function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let displayValue = 0;
 let value = 0;
+let operation;
 
 let display = document.getElementById('display');
 let displayNumber = document.createTextNode(displayValue);
@@ -7,22 +8,46 @@ display.appendChild(displayNumber);
 
 let numbers = document.querySelectorAll('.numbers');
 numbers.forEach(number => {
-    number.addEventListener('click', function() {
-        if (displayValue === 0) {
-            display.removeChild(displayNumber);
-            displayValue = this.id;
-            displayNumber = document.createTextNode(displayValue);
-            display.appendChild(displayNumber);
-            value += displayValue;
-        } else {
-            displayValue = this.id;
-            displayNumber = document.createTextNode(displayValue);
-            display.appendChild(displayNumber);
-            console.log(displayNumber.nodeValue);
-            value += displayValue;
-        }    
-    })
+    number.addEventListener('click', showNumbers);
 });
+
+function showNumbers() {
+    if (displayValue === 0) {
+        display.removeChild(displayNumber);
+        displayValue = this.id;
+        displayNumber = document.createTextNode(displayValue);
+        display.appendChild(displayNumber);
+        value += displayValue;
+    } else {
+        displayValue = this.id;
+        displayNumber = document.createTextNode(displayValue);
+        display.appendChild(displayNumber);
+        value += displayValue;
+    }    
+}
+
+let clear = document.getElementById('clear');
+clear.addEventListener('click', clearDisplay);
+
+function clearDisplay() {
+    display.innerText = '';
+    displayNumber.nodeValue = 0;
+    displayValue = 0;
+    value = 0;
+    display.appendChild(displayNumber);
+}
+
+function operate() {
+    if (operation === "+") {
+        add();
+    } else if (operation === "-") {
+        subtract();
+    } else if (operation === "*") {
+        multiply();
+    } else if (operation === "/") {
+        divide();
+    }
+}
 
 function add(a, b) {
     return a + b;
@@ -38,17 +63,4 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
-}
-
-function operate() {
-    let operation;
-    if (operation === "+") {
-        add();
-    } else if (operation === "-") {
-        subtract();
-    } else if (operation === "*") {
-        multiply();
-    } else if (operation === "/") {
-        divide();
-    }
 }
